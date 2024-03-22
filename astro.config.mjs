877ae546dcbb116be292df2392500735b3e46594
Cédric Sazos <cedric.sazos@tutanota.com>
2024-03-22 17:09:57 +0100

    added node as adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
 import alpine from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpine()],
-  output: 'server'
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
